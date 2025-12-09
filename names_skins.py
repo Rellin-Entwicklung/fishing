5dd92978327f25f482376d499c9be2aa7e09d8f4
Stephan Rellin <mrrellinst@gmail.com>
2025-12-09 10:20:29 +0100

    initial_commit

--- names_skins.py
+++ names_skins.py
@@ -2,8 +2,8 @@ from tkinter.font import names
 
 skins= ["brown_trout", "rainbow_trout","char",
         "natural_roach", "gold_fisch", "Koi_carp", "crucian_carp",
-        "natural_perch", firetiger]
+        "natural_perch", "firetiger"]
 
 starterkits= ["brown_trout", "rainbow_trout","char",
         "natural_roach", "gold_fisch", "Koi_carp", "crucian_carp",
-        "natural_perch", firetiger]
+        "natural_perch", "firetiger"]
